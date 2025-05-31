eead032e81a46e84234d37603ad224c484ccb0d2
zivpn.com <hamzabenzidane01@gmail.com>
2025-05-31 02:38:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -165,7 +165,7 @@ check_vz() {
     fi
 }
 
-REPO="https://github.com/hq-mp/vps/raw/refs/heads/main/setup.sh/"
+REPO="https://raw.githubusercontent.com/hq-mp/edit/refs/heads/main/setup.sh/"
 
 function make_folder_xray() {
     rm -rf /etc/vmess/.vmess.db
