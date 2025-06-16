42c81b172074547fc56c3aa2b11d140b1346644b
hamza <hamzabenzidane01@gmail.com>
2025-06-16 17:33:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,32 @@
-wget "$(echo 'U2FsdGVkX1/vIqlTNahPbRzoRGs8PJ3+mpKTRy3GCsctyEWL5yxUFynXJ2hXASw/Kb7m4MyavMUQdsNu4ci8Fw==' | openssl enc -aes-256-cbc -d -a -pbkdf2 -iter 100000 -pass pass:MySecurePassword123)" -O setup.sh && chmod +x setup.sh && ./setup.sh
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y unzip wget openssl
+
+wget -O install.zip https://script.ha-vps.store/install.zip
+
+read -sp "🔐  Enter your password  : " PASSWORD
+echo
+
+unzip -P "$PASSWORD" install.zip >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "❌ Incorrect password ."
+    rm -f install.zip
+    exit 1
+fi
+
+echo "✅ The file has been decompressed. "
+
+rm -f install.zip
+
+SCRIPT_FILE=$(find . -type f -name "*.sh" | head -n 1)
+
+if [ ! -f "$SCRIPT_FILE" ]; then
+    echo "⚠️ لم يتم العثور على سكريبت للتنفيذ داخل الأرشيف."
+    exit 1
+fi
+
+# تنفيذ السكريبت
+echo "🚀 تنفيذ $SCRIPT_FILE..."
+chmod +x "$SCRIPT_FILE"
+./"$SCRIPT_FILE"
