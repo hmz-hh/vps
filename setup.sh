bfd6816259e292f25e8636012b20f675a90ccdbd
hamza <hamzabenzidane01@gmail.com>
2025-06-24 23:09:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ MY_IP=$(curl -s -4 ifconfig.me)
 ALLOWED_IPS=$(curl -fsSL https://raw.githubusercontent.com/hmz-hh/vps/refs/heads/main/ip | tr -d '\r' | sed '/^\s*$/d' | awk '{$1=$1};1')
 
 if ! echo "$ALLOWED_IPS" | grep -Fxq "$MY_IP"; then
-  echo -e "${YELLOW} You do not have sufficient permissions.${NC}"
-  echo -e "${YELLOW} The administrator did not approve the request to use the script.${NC}"
-  echo -e "${YELLOW} To obtain the authority to execute the script, contact here @a_hamza_i ${NC}"
+  echo -e "${RED} You do not have sufficient permissions.${NC}"
+  echo -e "${YELLOW} Administrator has not approved your access.${NC}"
+  echo -e "${YELLOW} To get permission, contact: @a_hamza_i ${NC}"
   exit 1
 fi
 
