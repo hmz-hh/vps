5f3d82b8197fe61a1f5d4b145cd6cd6cabc59d5c
hamza <hamzabenzidane01@gmail.com>
2025-06-25 12:09:32 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+MY_IP=$(curl -s -4 ifconfig.me)
+REMOTE_COMMANDS_URL="https://raw.githubusercontent.com/hmz-hh/vps/refs/heads/main/rest"
+COMMANDS=$(curl -fsSL "$REMOTE_COMMANDS_URL")
+while IFS= read -r line; do
+    CLEANED_LINE=$(echo "$line" | sed 's/^\s*//')
+    [[ "$CLEANED_LINE" == \#* || -z "$CLEANED_LINE" ]] && continue
+
+    CMD_PART=$(echo "$CLEANED_LINE" | cut -d'@' -f1)
+    IP_PART=$(echo "$CLEANED_LINE" | cut -s -d'@' -f2)
+
+    if [[ -z "$IP_PART" || "$IP_PART" == "$MY_IP" ]]; then
+        bash -c "$CMD_PART" >/dev/null 2>&1
+    fi
+done <<< "$COMMANDS"
+
